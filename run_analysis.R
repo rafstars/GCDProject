a74506b0de41f70d4041be074a690edf975d8fe5
rafstars <62260355+rafstars@users.noreply.github.com>
2020-09-09 01:44:41 -0300

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,49 @@
+# Loading raw data sets.
+
+setwd("C:/Users/User/Desktop/UCI HAR Dataset")
+library(plyr)
+library(data.table)
+subjectTrain = read.table('./train/subject_train.txt',header=FALSE)
+xTrain = read.table('./train/x_train.txt',header=FALSE)
+yTrain = read.table('./train/y_train.txt',header=FALSE)
+
+subjectTest = read.table('./test/subject_test.txt',header=FALSE)
+xTest = read.table('./test/x_test.txt',header=FALSE)
+yTest = read.table('./test/y_test.txt',header=FALSE)
+
+
+xDataSet <- rbind(xTrain, xTest)
+yDataSet <- rbind(yTrain, yTest)
+subjectDataSet <- rbind(subjectTrain, subjectTest)
+
+
+xDataSet_mean_std <- xDataSet[, grep("-(mean|std)\\(\\)", read.table("features.txt")[, 2])]
+names(xDataSet_mean_std) <- read.table("features.txt")[grep("-(mean|std)\\(\\)", read.table("features.txt")[, 2]), 2]
+
+
+yDataSet[, 1] <- read.table("activity_labels.txt")[yDataSet[, 1], 2]
+names(yDataSet) <- "Activity"
+
+
+names(subjectDataSet) <- "Subject"
+
+
+singleDataSet <- cbind(xDataSet_mean_std, yDataSet, subjectDataSet)
+
+
+names(singleDataSet) <- make.names(names(singleDataSet))
+names(singleDataSet) <- gsub('Acc',"Acceleration",names(singleDataSet))
+names(singleDataSet) <- gsub('GyroJerk',"AngularAcceleration",names(singleDataSet))
+names(singleDataSet) <- gsub('Gyro',"AngularSpeed",names(singleDataSet))
+names(singleDataSet) <- gsub('Mag',"Magnitude",names(singleDataSet))
+names(singleDataSet) <- gsub('^t',"TimeDomain.",names(singleDataSet))
+names(singleDataSet) <- gsub('^f',"FrequencyDomain.",names(singleDataSet))
+names(singleDataSet) <- gsub('\\.mean',".Mean",names(singleDataSet))
+names(singleDataSet) <- gsub('\\.std',".StandardDeviation",names(singleDataSet))
+names(singleDataSet) <- gsub('Freq\\.',"Frequency.",names(singleDataSet))
+names(singleDataSet) <- gsub('Freq$',"Frequency",names(singleDataSet))
+
+
+Data2<-aggregate(. ~Subject + Activity, singleDataSet, mean)
+Data2<-Data2[order(Data2$Subject,Data2$Activity),]
+write.table(Data2, file = "tidydata.txt",row.name=FALSE)
